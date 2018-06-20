98b6108025ee44b5ab310d5d2654ebf43f1341a5
kunle babatunde <kb2232@nyu.edu>
2018-06-20 07:23:02 -0400

    updates

--- src/index.js
+++ src/index.js
@@ -1,8 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Timer from './components/timer';
+import './styles.css';
+import { BrowserRouter as Router, Link, Route} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Game extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			title: 'Totally Trivial Trivia!!',
+      isHidden: true,
+      isToggle: true,
+		};
+	}
+  
+  handleClick() {
+    this.setState({
+      isToggleOn: !this.state.isToggleOn,
+      isHidden: !this.state.isHidden,
+    });
+  }
+
+	render() {
+		return (
+			<Router>
+				<div>
+					<div className="pb-2 mt-4 mb-2 title">{this.state.title}</div>
+					<Link to="/startGame">
+						<button onClick={this.handleClick.bind(this)} className="btn btn-danger">
+            {!this.state.isToggleOn ? 'ON' : 'OFF'}
+						</button>
+					</Link>
+					{/*Routes will go here*/}
+					<Route
+						path="/startGame"
+						render={() => {
+							return (
+								<div className="timer">
+                {!this.state.isHidden && <Timer />}
+								</div>
+							);
+						}}
+					/>
+				</div>
+			</Router>
+		);
+	}
+}
+
+ReactDOM.render(<Game />, document.getElementById('game'));
